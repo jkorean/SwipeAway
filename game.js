b4be786a8ec051335efc8de9568b26b3fdd21cc1
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2016-05-07 11:33:07 -0700

    Finished initialization for all screen sizes

--- game.js
+++ game.js
@@ -25,8 +25,24 @@ function move() {
 	});
 }
 
-function initialize(lanes) {
-	if(screen.width > screen.height) {
-		alert('Success');
+function initialize() {
+	if (screen.height <= 800 && screen.width <= 800) {
+		$("div#desktop").hide();
+		$("div#container").css({"width": "100vw", 
+								"height": "100vh"});
+
+		$width = $("div#container").width();
+		$height = $("div#container").height();
+		$("div#container").css("height", $height - 2 + "px");
+	} else {
+		$("div#container").css({"height": "400px", "width": "750px",
+								"border-width": "1px", "margin": "0 auto"});
+		$width = $("div#container").width();
+		$height = $("div#container").height();
 	}
+
+	$("div#ui").css("height", (0.075 * $height) - 2 + "px");
+	$game = $height - $("div#ui").height() - 2;
+	$laneHeight = ($game / lanes) - 2;
+	$("div.track").css("height", $laneHeight);
 }

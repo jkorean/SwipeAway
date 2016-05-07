fe54f9fe2a3a374a17617529b1ba629483690830
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2016-05-07 15:44:39 -0700

    Added screen rotation responsiveness, fixed obstacle generation.

--- game.js
+++ game.js
@@ -2,30 +2,47 @@ $(document).ready(function() {
 	initialize();
 	generate(1);
 	generate(2);
-	setInterval('move();', 5);
+	setInterval('move();', $tickLength);
 	
 });
 
-function generate(track) {
-	var trackId = "#t" + track;
-	var block = $('<div></div>');
-	block.addClass("obstacle");
+// Recalls intialize function if screen orientation is changed.
+$(window).on("orientationchange", initialize);
 
-	var trackHeight = $(trackId).css("height");
-	block.css({"height": trackHeight, "left": "450px"});
-	$(trackId).append(block);
+// Generates a new obstacle off-screen, to the right.
+function generate(track) {
+	$trackId = "#t" + track;
+	$block = $('<div></div>');
+	$block.addClass("obstacle");
+
+	$trackHeight = $($trackId).css("height");
+	$leftInit = parseInt($("div#container").css("margin-left")) + $("div#container").width() + 2;
+
+	$block.css({"height": $trackHeight, "left": $leftInit});
+	$($trackId).append($block);
 }
 
+// Moves all obstacles by 1 pixel.
+// **PENDING** Checks all obstacles to see if they have collided with an object.
 function move() {
-	var blocks = $(".obstacle");
+	$blocks = $(".obstacle");
+	$offLeft = parseInt($("div#container").css("margin-left")) - 10;
 
-	blocks.each(function() {
-		var newLeft = parseInt($(this).css("left")) - 1;
-		$(this).css("left", newLeft + "px");
+	$blocks.each(function() {
+		$newLeft = parseInt($(this).css("left")) - 1;
+		$(this).css("left", $newLeft + "px");
+
+		// Deletes any obstacles that have travelled to the right off screen.
+		if ($newLeft <= $offLeft) {
+			$(this).remove();
+		}
 	});
 }
 
+// Sets up appropriate game screen depending on screen size.
 function initialize() {
+	$("p#portError").hide();
+	// Sets up screen for phones or small devices.
 	if (screen.height <= 800 && screen.width <= 800) {
 		$("div#desktop").hide();
 		$("div#container").css({"width": "100vw", 
@@ -33,7 +50,14 @@ function initialize() {
 
 		$width = $("div#container").width();
 		$height = $("div#container").height();
+		// If the phone is in portrait mode, limits the height of game pane and displays error.
+		if ($height > $width) {
+			$height = $width / 1.5;
+			$("#portError").show();
+		}
+
 		$("div#container").css("height", $height - 2 + "px");
+	// Sets up screen for a PC.
 	} else {
 		$("div#container").css({"height": "400px", "width": "750px",
 								"border-width": "1px", "margin": "0 auto"});
@@ -41,8 +65,9 @@ function initialize() {
 		$height = $("div#container").height();
 	}
 
+	// Sets height of UI bar and lanes.
 	$("div#ui").css("height", (0.075 * $height) - 2 + "px");
 	$game = $height - $("div#ui").height() - 2;
-	$laneHeight = ($game / lanes) - 2;
-	$("div.track").css("height", $laneHeight);
+	$laneHeight = ($game / $lanes) - 2;
+	$("div.track").css({"height": $laneHeight, "width": $width});
 }
